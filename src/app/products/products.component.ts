61ad238232a2c1ae42e8376831b9ed9e5a659c57
aymensellaouti <aymen.sellaouti@gmail.com>
2025-03-11 17:17:59 +0100

    products exercice correction

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import {
   BehaviorSubject,
   Observable,
@@ -6,19 +6,48 @@ import {
   map,
   takeWhile,
   scan,
+  skip,
 } from "rxjs";
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
 import { Settings } from "./dto/product-settings.dto";
 
 @Component({
-  selector: "app-products",
-  templateUrl: "./products.component.html",
-  styleUrls: ["./products.component.css"],
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
+  setting = { skip: 0, limit: 12 };
+  settings$ = new BehaviorSubject(this.setting);
+  productService = inject(ProductService);
+  isDisabled = false;
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
+  products$: Observable<Product[]> = this.settings$.pipe(
+    // {0,12}, {12,1é}, {12,24} ...
+    concatMap((setting) => this.productService.getProducts(setting)),
+    // ApiResponse1, ApiResponse2, ApiResponse3
+    map((productApiResponse) => productApiResponse.products),
+    // [products]
+    takeWhile((products) => {
+      if (!products.length) {
+        this.isDisabled = true;
+        return false;
+      }
+      return true;
+    }),
+    scan((oldProducts, newProducts) => [...oldProducts, ...newProducts])
+  );
+
+  // [oldProducts, newProducts]
   constructor() {}
+
+  moreProducts() {
+    this.setting = {
+      ...this.setting,
+      skip: this.setting.skip + this.setting.limit,
+    };
+    this.settings$.next(this.setting);
+  }
 }
