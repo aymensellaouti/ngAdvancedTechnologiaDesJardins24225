d25435f3b70be3d3fd03d43a462c6b01d62e9c1d
aymensellaouti <aymen.sellaouti@gmail.com>
2025-03-11 18:54:38 +0100

    correction autocomplete

--- src/app/cv/autocomplete/autocomplete.component.ts
+++ src/app/cv/autocomplete/autocomplete.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from "@angular/core";
 import { FormBuilder, AbstractControl } from "@angular/forms";
-import { debounceTime, distinctUntilChanged, switchMap, tap } from "rxjs";
+import { debounceTime, distinctUntilChanged, Observable, switchMap, tap } from "rxjs";
 import { CvService } from "../services/cv.service";
+import { Cv } from "../model/cv";
 
 @Component({
   selector: "app-autocomplete",
@@ -15,4 +16,10 @@ export class AutocompleteComponent {
     return this.form.get("search")!;
   }
   form = this.formBuilder.group({ search: [""] });
+  cvs$: Observable<Cv[]> = this.search.valueChanges.pipe(
+    debounceTime(500),
+    distinctUntilChanged(),
+    tap( data => console.log(data)),
+    switchMap(name => this.cvService.selectByName(name)),
+  );
 }
