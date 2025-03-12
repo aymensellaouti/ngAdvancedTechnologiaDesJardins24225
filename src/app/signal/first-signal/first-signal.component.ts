af5c7bff2d7d2122255fe9be007896af83648bfa
aymensellaouti <aymen.sellaouti@gmail.com>
2025-03-12 20:31:47 +0100

    exemples API du Signal

--- src/app/signal/first-signal/first-signal.component.ts
+++ src/app/signal/first-signal/first-signal.component.ts
@@ -2,14 +2,20 @@ import { Component, computed, signal, WritableSignal } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-first-signal',
-    templateUrl: './first-signal.component.html',
-    styleUrl: './first-signal.component.css',
-    standalone: true,
-    imports: [ReactiveFormsModule, FormsModule],
+  selector: 'app-first-signal',
+  templateUrl: './first-signal.component.html',
+  styleUrl: './first-signal.component.css',
+  standalone: true,
+  imports: [ReactiveFormsModule, FormsModule],
 })
 export class FirstSignalComponent {
-  x = 5;
-  y = 7;
-  z = this.x + this.y;
+  x = signal(5);
+  y = signal(7);
+  z = computed(() => this.x() + this.y());
+  doubleZ = computed(() => this.z() * 2);
+
+  doubleMoiLeSignal(monSignal: WritableSignal<number>) {
+    monSignal.update((valuerActuelleDuSignal) => valuerActuelleDuSignal * 2)
+  }
+
 }
